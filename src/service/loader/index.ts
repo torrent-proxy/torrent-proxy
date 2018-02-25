6c9ca2170dcd8534f6fdb8b5747061b846ddf4d6
kicumkicum <kicumkicum@gmail.com>
2018-02-26 01:06:27 +0700

    Use torrent-stream-manager. Remove using peerflix

--- src/service/loader/index.ts
+++ src/service/loader/index.ts
@@ -1,17 +1,18 @@
-import { ChildProcess } from 'child_process';
 import { adapters, Manager } from 'tracker-proxy';
-import { spawn } from 'child_process';
+import { File, MagnetInfo } from './i-torrent-stream';
+import TorrentStreamManager from './torrent-stream-manager';
 
 export default class TorrentProxyServer {
 	private config: any;
+	private torrentStreamManager: TorrentStreamManager;
 	private trackerManager: Manager;
-	private peerflix: ChildProcess | null;
 
 	constructor(config) {
-		this.peerflix = null;
 		this.config = config;
 
+		this.torrentStreamManager = new TorrentStreamManager();
 		this.trackerManager = new Manager();
+
 		this.trackerManager.include(new adapters.TPB());
 	}
 
@@ -36,42 +37,16 @@ export default class TorrentProxyServer {
 		});
 	}
 
-	load(magnet: string): Promise<string> {
-		return new Promise((resolve, reject) => {
-			return this.cancel()
-				.then(() => {
-					const localIP = this.config.ip;
-					const port = this.config.port;
-					this.peerflix = spawn('peerflix', ['--hostname', localIP, '--port', port, magnet]);
-					this.peerflix.on('error', (err) => reject(err));
+	loadMagnetInfo(magnet: string): Promise<MagnetInfo> {
+		// TODO: Получить hash из magnet
+		const hash = magnet;
 
-					let resolved = false;
-
-					this.peerflix.stdout.on('data', () => {
-						if (resolved) {
-							return;
-						}
-						resolved = true;
-						resolve('http://' + localIP + ':' + port);
-					});
-
-					this.peerflix.stderr.on('data', (err) => {
-						if (err) {
-							reject(err);
-						}
-					});
-				});
-		});
+		const torrentStream = this.torrentStreamManager.createTorrentStream(hash);
+		return torrentStream.load();
 	}
 
-	cancel(): Promise<void> {
-		if (!this.peerflix) {
-			return Promise.resolve();
-		}
+	// TODO: что использовать? Объект file или magnet и filePath?
+	downloadFile(file: File) {
 
-		this.peerflix.kill('SIGTERM');
-		this.peerflix = null;
-
-		return Promise.resolve();
 	}
 }
