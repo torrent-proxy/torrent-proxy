58f578ed01d43610902e1e9bfa25825d301f9a84
kicumkicum <kicumkicum@gmail.com>
2019-09-22 21:02:51 +0300

    Add route for btfs

--- src/service/api/router/routes/rnd.ts
+++ src/service/api/router/routes/rnd.ts
@@ -1,5 +1,12 @@
 import * as fs from 'fs';
 import * as torrentStream from 'torrent-stream';
+import {promisify} from 'util';
+
+const BTStream = require('bt-stream');
+const magnetURIDecode = require('magnet-uri');
+
+const fsStat = promisify(fs.stat);
+const fsMkdir = promisify(fs.mkdir);
 
 let engine = null;
 
@@ -33,6 +40,34 @@ export default () => {
 			const file = engine.files.find((file) => file.path === incomingMessage.params.filePath);
 			const stream = file.createReadStream();
 			stream.pipe(res);
+		},
+		downloadWithBTFS: (incomingMessage, res) => {
+			const {filePath, magnet} = incomingMessage.params;
+			const hash = magnetURIDecode(magnet).infoHash;
+			const mountPoint = join(`/Users/oleg/mounts/`, hash);
+
+			console.log({filePath, magnet});
+
+			return fsStat(mountPoint)
+				.catch((err) => {
+					if (err.code === `ENOENT`) {
+						return fsMkdir(mountPoint);
+					} else if(err.code === `EEXIST`) {
+						// TODO: hz
+					}
+				})
+				.then(() => mount({magnet, mountPoint}))
+				.then(() => {
+					const p = join(mountPoint, filePath);
+					console.log({path: p});
+
+					const stream = fs.createReadStream(p);
+					stream.pipe(res);
+				})
+				.catch((err) => {
+					console.error({err});
+					process.exit(1);
+				});
 		},
 		get3: (incomingMessage, res) => {
 			res.send(fs.readFileSync(__dirname + '/../../../../../../index.html'));
