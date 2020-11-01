20ea2d9bae3631ea06e7e9d2882d71979eabc2b0
Анна Полуэктова <a.poluektova88@gmail.com>
2020-11-01 15:35:19 +0400

    Remove unnecessary dependencies, fix editorconfig and tsconfig

--- src/components/color-theme/color-theme.component.tsx
+++ src/components/color-theme/color-theme.component.tsx
@@ -2,12 +2,10 @@ import * as React from "react";
 
 const ColorTheme: React.FunctionComponent = () => {
   return (
-    <React.Fragment>
       <div className="color-theme__wrapper">
         <span className="color-theme__text color-theme__text--light">Light |</span>
         <span className="color-theme__text color-theme__text--dark" > Dark</span>
       </div>
-    </React.Fragment>
   );
 };
 
