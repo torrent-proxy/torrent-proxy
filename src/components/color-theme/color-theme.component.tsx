638f66700c47c13cb9349b67b70043ab62fe2d20
Анна Полуэктова <a.poluektova88@gmail.com>
2020-11-07 20:06:34 +0400

    Fix imports in index.tsx and add props for ColorTheme

--- src/components/color-theme/color-theme.component.tsx
+++ src/components/color-theme/color-theme.component.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-const ColorTheme: React.FunctionComponent = () => {
+interface Props {
+  onColorClick: () => void;
+}
+
+const ColorTheme: React.FunctionComponent<Props> = (props: Props) => {
+  const {onColorClick} = props;
+
   return (
     <div className="color-theme__wrapper">
-      <span className="color-theme__text color-theme__text--light">Light |</span>
+      <span
+      className="color-theme__text color-theme__text--light"
+      onClick={onColorClick}
+      >Light |</span>
       <span className="color-theme__text color-theme__text--dark" > Dark</span>
     </div>
   );
